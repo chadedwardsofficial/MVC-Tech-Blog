2a7f19929c7dc25128e6be0ae07f0ad534ed2c96
Chad Edwards <chadedwardsofficial@gmail.com>
2023-12-10 12:48:54 -0600

    added basic route for homepages to render all blogs

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,27 @@
+const router = require('express').Router();
+const { Blog, User } = require('../models');
+const withAuth = require('../utils/auth');
+
+// When you log in all of the Blogs Will be Rendered to the Home Page. 
+router.get('/', async (req, res) => {
+  try {
+    const blogData = await Blog.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    const blogs = blogData.map((blog) => blog.get({ plain: true }));
+
+    res.render('homepage', { 
+      blogs, 
+      logged_in: req.session.logged_in 
+    });
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
