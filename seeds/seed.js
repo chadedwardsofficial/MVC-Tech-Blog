6cc35050d8fd97dcb54bbcc985504766bc09c11c
Chad Edwards <chadedwardsofficial@gmail.com>
2023-12-12 13:05:00 -0600

    added handlebars layout and styling for blog and homepage

--- seeds/seed.js
+++ seeds/seed.js
@@ -5,7 +5,7 @@ const userData = require('./userData.json');
 const blogData = require('./blogData.json');
 
 const seedDatabase = async () => {
-  await sequelize.sync({ force: true });
+  await sequelize.sync({ force: false });
 
   const users = await User.bulkCreate(userData, {
     individualHooks: true,
